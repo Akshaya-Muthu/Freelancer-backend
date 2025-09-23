e49cfd963d97e883b3d7adc8ed8ad27fc2264de1
Akshaya Muthu <129148020+Akshaya-Muthu@users.noreply.github.com>
2025-09-23 13:02:31 +0530

    Update userAuthRoutes.js

--- routes/userAuthRoutes.js
+++ routes/userAuthRoutes.js
@@ -2,9 +2,12 @@ import express from 'express';
 import { isProtected } from '../middlewares/authMiddleware.js';
 import { isAdmin } from '../middlewares/adminMiddleware.js';
 const router = express.Router();
-import { registerUser, loginUser, logoutUser , changePassword} from '../controllers/userAuthController.js';
+import { registerUser, loginUser, logoutUser ,forgotPassword,resetPassword, changePassword} from '../controllers/userAuthController.js';
 router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
 router.route('/change-password').post(isProtected,changePassword);
 router.route('/logout').get(logoutUser);
+router.route('/forgot-password').post(forgotPassword);
+router.route('/reset-password/:token').post(resetPassword);
+
 export default router;
